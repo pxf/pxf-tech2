d75f3a2e29e72e544048a0d03a97e800b58cb2cb
Johannes Algelind <jalgelind@gmail.com>
2010-07-10 00:59:11 +0200

    Removed SampleLibrary

--- Libraries/SampleLibrary/SampleLibrary.lua
+++
@@ -1,6 +0,0 @@
-Import("../../PxfBuild.lua")
-
-library = NewLibrary("SampleLibrary")
-library:AddSystemLibrary("ole")
-library:AddIncludeDirectory("Include")
-library:AddSourceDirectory("Source/*.cpp")
