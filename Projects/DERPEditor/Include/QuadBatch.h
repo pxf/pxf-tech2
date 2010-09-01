77be607e141ad3e0918730d0ca14a148623338da
sweetfish <sweetfish@gmail.com>
2010-09-01 20:56:34 +0200

    Forgot two files.

---
+++ Projects/DERPEditor/Include/QuadBatch.h
@@ -0,0 +1,52 @@
+#ifndef __DERPEDITOR_QUADBATCH_H__
+#define __DERPEDITOR_QUADBATCH_H__
+
+#include <Pxf/Graphics/VertexBuffer.h>
+
+namespace DERPEditor
+{
+    class QuadBatch
+    {
+    public:
+        
+        struct QuadVertex
+        {
+            Vec3f position;
+            Vec2f coord;
+            Vec4f color;
+        };
+        
+        QuadBatch(int _size);
+        ~QuadBatch();
+        
+        /*void SetColor(float r, float g, float b);
+        void SetAlpha(float a);
+		void SetTextureSubset(float tl_u, float tl_v, float br_u, float br_v);
+		void SetRotation(float angle); // Rotate following quad around its own axis
+		void Rotate(float angle); // Rotate coord system
+        void Translate(float x, float y); // Translate coord system
+        void LoadIdentity(); // Reset coord system
+        */
+        void SetDepth(float d) { m_CurrentDepth = d; };
+		
+		void Reset();
+		/*void AddFreeform(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3);
+		void AddTopLeft(float x, float y, float w, float h);
+		*/
+		void AddCentered(float x, float y, float w, float h);
+		void Draw();
+		
+	private:
+        Pxf::Graphics::VertexBuffer *m_VertexBuffer;
+        QuadVertex* m_pVertBuf;
+        int m_VertBufSize;
+        
+        int m_CurrentVert;
+        
+        float m_CurrentDepth;
+        Vec2f m_CurrentCoords[4];
+        Vec4f m_CurrentColor;
+    };
+} /* DERPEditor */
+
+#endif /* __DERPEDITOR_QUADBATCH_H__ */
---
+++ Projects/DERPEditor/Source/QuadBatch.cpp
@@ -0,0 +1,61 @@
+#include "QuadBatch.h"
+
+#include <Pxf/Kernel.h>
+#include <Pxf/Base/Debug.h>
+#include <Pxf/Base/Utils.h>
+
+#include <Pxf/Graphics/GraphicsDevice.h>
+#include <Pxf/Graphics/VertexBufferDefs.h>
+
+using namespace DERPEditor;
+using namespace Pxf::Graphics;
+
+QuadBatch::QuadBatch(int _size)
+{
+    m_VertBufSize = _size;
+    m_VertexBuffer = Pxf::Kernel::GetInstance()->GetGraphicsDevice()->CreateVertexBuffer(VB_LOCATION_GPU, VB_USAGE_DYNAMIC_DRAW);
+    m_VertexBuffer->CreateNewBuffer(_size, sizeof(QuadVertex) ); // pos = 3, tex coords = 2, colors = 4
+    m_pVertBuf = (QuadVert*)m_VertexBuffer->MapData(VB_ACCESS_WRITE_ONLY);
+    
+    Reset();
+}
+
+QuadBatch::~QuadBatch()
+{
+    Pxf::Kernel::GetInstance()->GetGraphicsDevice()->DestroyVertexBuffer(m_VertexBuffer);
+}
+
+void QuadBatch::Reset()
+{
+    m_CurrentVert = 0;
+    
+    m_CurrentColor = Vec4f(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+void QuadBatch::Draw()
+{
+    Pxf::Kernel::GetInstance()->GetGraphicsDevice()->DrawBuffer(m_VertexBuffer, m_CurrentVert);
+}
+
+void QuadBatch::AddCentered(float x, float y, float w, float h)
+{
+    float w2 = w / 2.0f;
+    float h2 = h / 2.0f;
+    m_pVertBuf[m_CurrentVert].position = Vec3(x-w2,y-h2,m_CurrentDepth);
+    m_pVertBuf[m_CurrentVert].coords = m_CurrentCoords[0];
+    m_pVertBuf[m_CurrentVert].color = m_CurrentColor;
+    
+    m_pVertBuf[m_CurrentVert+1].position = Vec3(x+w2,y-h2,m_CurrentDepth);
+    m_pVertBuf[m_CurrentVert+1].coords = m_CurrentCoords[1];
+    m_pVertBuf[m_CurrentVert+1].color = m_CurrentColor;
+    
+    m_pVertBuf[m_CurrentVert+2].position = Vec3(x+w2,y+h2,m_CurrentDepth);
+    m_pVertBuf[m_CurrentVert+2].coords = m_CurrentCoords[2];
+    m_pVertBuf[m_CurrentVert+2].color = m_CurrentColor;
+    
+    m_pVertBuf[m_CurrentVert+3].position = Vec3(x-w2,y+h2,m_CurrentDepth);
+    m_pVertBuf[m_CurrentVert+3].coords = m_CurrentCoords[3];
+    m_pVertBuf[m_CurrentVert+3].color = m_CurrentColor;
+    
+    m_CurrentVert = m_CurrentVert + 4;
+}
