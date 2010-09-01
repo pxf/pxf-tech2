f4d2925e2f2764fcacb7dc70a9924237aef8ffb6
jalgelind <jalgelind@gmail.com>
2010-09-01 17:23:07 +0200

    Updated enet.

--- Libraries/enet/enet.lua
+++ Libraries/enet/enet.lua
@@ -5,6 +5,9 @@ library = NewLibrary("enet")
 if family == "windows" then
     library:AddSystemLibrary("ws2_32")
     library:AddSystemLibrary("winmm")
+    library:AddSourceDirectory("sdk/src/win32/*.c")
+else
+    library:AddSourceDirectory("sdk/src/unix/*.c")
 end
 
 library:AddIncludeDirectory("sdk/include")
