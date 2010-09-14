0dd5296a572fd908f24986409903ded8ef3a5611
jalgelind <jalgelind@gmail.com>
2010-09-14 15:26:50 +0200

    Honk tonk compilation

--- Libraries/sstat/sstat.lua
+++ Libraries/sstat/sstat.lua
@@ -1,5 +1,6 @@
 Import("../../PxfBuild.lua")
 
 library = NewLibrary("sstat")
+library:AddSystemLibrary("Psapi")
 library:AddIncludeDirectory("sdk/include")
-library:AddSourceDirectory("sdk/src/*.c")
+library:AddSourceDirectory("sdk/src/*.cpp")
