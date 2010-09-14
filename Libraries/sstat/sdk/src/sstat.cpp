7a8865153ea10c221fbd4114d7c557ca97c0ee63
sweetfish <sweetfish@gmail.com>
2010-09-14 17:19:22 +0200

    Added mem usage code for linux.

--- Libraries/sstat/sdk/src/sstat.cpp
+++ Libraries/sstat/sdk/src/sstat.cpp
@@ -54,7 +54,58 @@ long int sstat_memoryusage()
 	return info.resident_size;
 }
 
+#elif defined (__LINUX__) || defined (__linux__)
+
+#include <unistd.h>
+#include <ios>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+//void process_mem_usage(double& vm_usage, double& resident_set)
+long int sstat_memoryusage()
+{
+   using std::ios_base;
+   using std::ifstream;
+   using std::string;
+
+   double vm_usage     = 0.0;
+   double resident_set = 0.0;
+   long int ret_val = 0;
+
+   // 'file' stat seems to give the most reliable results
+   //
+   ifstream stat_stream("/proc/self/stat",ios_base::in);
+
+   // dummy vars for leading entries in stat that we don't care about
+   //
+   string pid, comm, state, ppid, pgrp, session, tty_nr;
+   string tpgid, flags, minflt, cminflt, majflt, cmajflt;
+   string utime, stime, cutime, cstime, priority, nice;
+   string O, itrealvalue, starttime;
+
+   // the two fields we want
+   //
+   unsigned long vsize;
+   long rss;
+
+   stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr
+               >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt
+               >> utime >> stime >> cutime >> cstime >> priority >> nice
+               >> O >> itrealvalue >> starttime >> vsize >> rss; // don't care about the rest
+
+   long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+   vm_usage     = vsize / 1024.0;
+   //resident_set = rss * page_size_kb;
+
+   ret_val = rss * page_size_kb;
+
+   return ret_val;
+}
+
 #else
+
+
 long int sstat_memoryusage()
 {
 	return 420*1024*1024;
