3852e3d4972c7acaa16dcbd0be47732786927cb4
jalgelind <jalgelind@gmail.com>
2010-09-21 16:13:00 +0200

    json stuff

--- Projects/DERPRenderer/Source/BlockRenderer.cpp
+++ Projects/DERPRenderer/Source/BlockRenderer.cpp
@@ -1,7 +1,24 @@
 #include "BlockRenderer.h"
+#include <Pxf/Kernel.h>
+#include <Pxf/Base/String.h>
 #include <Pxf/Graphics/Texture.h>
+#include <Pxf/Resource/ResourceManager.h>
+#include <Pxf/Resource/Json.h>
+#include <json/json.h>
 
 using namespace Derp;
+using namespace Pxf;
+
+static Json::Value CreateJson(const char* data)
+{
+	Kernel* k = Kernel::GetInstance();
+	Resource::ResourceManager* res = k->GetResourceManager();
+	Resource::JsonLoader* jsonloader = res->FindResourceLoader<Pxf::Resource::JsonLoader>("json");
+	Resource::Json* doc = jsonloader->CreateFrom(data, StringLength(data));
+	Json::Value root = doc->GetRoot();
+	jsonloader->Destroy(doc);
+	return root;
+}
 
 bool AuxiliaryBlock::Initialize(const char* _JsonData)
 {
