50364166ae7309b582232f85b8f618e68c518a2b
sweetfish <sweetfish@gmail.com>
2010-09-22 12:11:44 +0200

    u mad?

---
+++ Projects/DERPRenderer/Include/Renderer.h
@@ -0,0 +1,49 @@
+#ifndef _DERPRENDERER_RENDERER_H_
+#define _DERPRENDERER_RENDERER_H_
+
+#include "RenderBlocks.h"
+
+#include <Pxf/Util/String.h>
+#include <Pxf/Util/Map.h>
+
+#include <Pxf/Resource/ResourceManager.h>
+#include <Pxf/Resource/Json.h>
+#include <Pxf/Resource/Text.h>
+
+#include <json/json.h>
+#include <json/writer.h>
+
+namespace Derp
+{
+  class Renderer
+  {
+  public:
+    Renderer(const char* _filepath);
+    virtual ~Renderer ();
+    
+    void LoadJson();
+    void BuildGraph();
+
+		void Execute();
+
+		Pxf::Util::Map<Pxf::Util::String, Block*> m_Blocks;
+		
+		Pxf::Graphics::Texture* GetResult()
+		{
+			return m_RootBlock->GetOutputValue();
+		}
+
+  private:
+    const char* m_Filepath;
+		Pxf::Resource::Json* m_doc;
+		Pxf::Resource::JsonLoader* m_jsonloader;
+		Json::Value root; // json-root
+		
+		// internal pipeline representation
+		const char* m_RootName;
+		RootBlock* m_RootBlock;
+		
+  };
+} /* Derp */
+
+#endif /* _DERPRENDERER_RENDERER_H_ */
---
+++ Projects/DERPRenderer/Source/Renderer.cpp
@@ -0,0 +1,101 @@
+#include "Renderer.h"
+
+#include <Pxf/Kernel.h>
+#include <Pxf/Base/String.h>
+#include <Pxf/Base/Debug.h>
+
+#include <Pxf/Graphics/Texture.h>
+
+using namespace Derp;
+using namespace Pxf;
+
+Renderer::Renderer(const char* _filepath)
+	: m_Filepath(_filepath)
+	, m_RootBlock(NULL)
+	, m_RootName(NULL)
+{
+  // do something?
+}
+
+Renderer::~Renderer()
+{
+  // clean up!
+	m_jsonloader->Destroy(m_doc);
+}
+
+void Renderer::LoadJson()
+{
+  Kernel* k = Kernel::GetInstance();
+
+ 	Resource::ResourceManager* res = k->GetResourceManager();
+  Resource::Text* data = res->Acquire<Resource::Text>(m_Filepath);
+	m_jsonloader = res->FindResourceLoader<Pxf::Resource::JsonLoader>("json");
+	m_doc = m_jsonloader->CreateFrom(data->Ptr(), StringLength(data->Ptr()));
+	
+	if (m_doc)
+	{
+		root = m_doc->GetRoot();
+		
+		// Loop through the array of blocks
+		for(int i = 0; i < root.size(); i++)
+		{
+			if(root[i]["blockType"].asString() == "PipelineTree")
+			{
+				m_RootName = root[i]["blockData"]["root"].asCString();
+				
+			} else if(root[i]["blockType"].asString() == "AuxComp") {
+				/*
+				 * Create AuxBlock
+				 */
+				Json::StyledWriter writer;
+				Block* newauxblock = new AuxiliaryBlock(this, writer.write(root[i]).c_str());
+				newauxblock->Initialize(&root[i]);
+				
+				// Add newly created aux block to the block list
+				m_Blocks.insert( std::make_pair(root[i]["blockName"].asString(), newauxblock));
+				
+				
+			} else if(root[i]["blockType"].asString() == "Root") {
+				/*
+				 * Create Root block
+				 */
+				Json::StyledWriter writer;
+				Block* newrootblock = new RootBlock(this, writer.write(root[i]).c_str());
+				newrootblock->Initialize(&root[i]);
+				
+				// Add newly created root block to the block list
+				m_Blocks.insert( std::make_pair(root[i]["blockName"].asString(), newrootblock));
+				
+			} else {
+				Message("Renderer", "Unknown block type: %s", root[i]["blockType"].asCString());
+			}
+		}
+		
+		
+	} else {
+    Message("Renderer", "Error while loading JSON data.");
+	}
+}
+
+void Renderer::BuildGraph()
+{
+	if (m_RootName)
+	{
+		m_RootBlock = (RootBlock*)m_Blocks[m_RootName];
+		m_RootBlock->BuildGraph();
+		
+	} else {
+		Message("Renderer", "Trying to build pipeline graph, but unknown root.");
+	}
+}
+
+void Renderer::Execute()
+{
+	if (m_RootBlock)
+	{
+		m_RootBlock->Execute();
+	} else {
+		Message("Renderer", "Failed to execute root block since it's NULL.");
+	}
+	
+}
