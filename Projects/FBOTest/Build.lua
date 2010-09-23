edc01443dfe74cdc1a2328be68618a4281b21285
Jhonnyg <jhonny@student.chalmers.se>
2010-09-23 21:11:41 +0200

    FUCK YOU FBO

---
+++ Projects/FBOTest/Build.lua
@@ -0,0 +1,17 @@
+Import("../../PxfBuild.lua")
+
+project = NewProject("FBOTest")
+
+--project.settings.debug = True
+--project.settings.static = True
+project:RequireLibrary("glfw")
+project:RequireLibrary("lua")
+project:RequireModule("pri")
+project:RequireModule("img")
+project:RequireModule("mesh")
+project:RequireModule("snd")
+project:RequireModule("json")
+project:RequireModule("net")
+project:AddIncludeDirectory("Include/")
+project:AddSourceDirectory("Source/*.cpp")
+project:Build()
