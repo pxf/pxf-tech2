053b06dea9c210abe990c32a561941d29b910182
Jhonnyg <jhonny@student.chalmers.se>
2010-10-07 01:03:25 +0200

    changed the look of the popup-menu a bit

--- Projects/DERPEditor/data/DERPWidgets.lua
+++ Projects/DERPEditor/data/DERPWidgets.lua
@@ -239,6 +239,62 @@ function derp:create_workspace_menu()
 		self:hide()
 	end
 	
+	function wid:draw(force)
+    if (self.redraw_needed or force) then
+      --gfx.translate(self.drawbox.x, self.drawbox.y)
+    
+		local old_a = gfx.getalpha()
+		
+		gfx.setalpha(0.25)
+		gfx.drawtopleft(self.drawbox.x+1,self.drawbox.y+1,self.drawbox.w-1,#self.menu * self.drawbox.h-1,0,511,128,1)
+		
+		
+		-- borders
+		gfx.drawtopleft(self.drawbox.x+1,self.drawbox.y,self.drawbox.w-1,1,5,5,1,1) -- top
+		gfx.drawtopleft(self.drawbox.x,self.drawbox.y+1,1,#self.menu * self.drawbox.h-1,5,5,1,1)
+		gfx.drawtopleft(self.drawbox.x+self.drawbox.w,self.drawbox.y+1,1,#self.menu *self.drawbox.h-1,5,5,1,1)
+		gfx.drawtopleft(self.drawbox.x+1,self.drawbox.y + self.drawbox.h * #self.menu,self.drawbox.w-1,1,5,5,1,1)
+        
+		gfx.setalpha(old_a)
+              
+      -- loop through all menu items
+	  
+	  gfx.translate(self.drawbox.x, self.drawbox.y)
+      local item_y = 0
+      for k,v in pairs(self.menu) do
+        if (k == self.highlightid) then
+          local r,g,b = gfx.getcolor()
+          
+		  old_a = gfx.getalpha()
+		  gfx.setalpha(0.5)
+		  gfx.setcolor(1.0,1.0,1.0)
+          gfx.drawtopleft(0, item_y, self.drawbox.w, self.itemheight,5,5,1,1)
+          gfx.setcolor(r,g,b)
+		  gfx.setalpha(old_a)
+        end
+        
+        -- item
+        gui:drawfont(v[1], 20, item_y + 12)
+        
+        -- short
+        if not (v[2].shortcut == nil) then
+          local r,g,b = gfx.getcolor()
+          gfx.setcolor(0.5, 0.5, 0.5)
+          gui:drawfont(v[2], self.stdwith-(#v[2]*8), item_y + 12)
+          gfx.setcolor(r,g,b)
+        elseif not (v[2].menu == nil) then
+          gui:drawfont(">", self.stdwith-8, item_y + 12)
+		elseif v[2].toggle then
+		  gui:drawfont("*",10,item_y+14)
+        end
+        
+        item_y = item_y + self.itemheight
+      end
+    
+      gfx.translate(-self.drawbox.x, -self.drawbox.y)
+    end
+  end
+	
 	--[[
 	function wid:draw(force)
 		if ((self.redraw_needed or force) and self.visible) then
