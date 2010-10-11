3f6d051c36c210bd6e575463f8c313d72756ee28
sweetfish <sweetfish@gmail.com>
2010-10-11 19:39:25 +0200

    Added some simple memory usage info in the about window.

--- Projects/DERPEditor/data/specwindows.lua
+++ Projects/DERPEditor/data/specwindows.lua
@@ -1,6 +1,16 @@
 function spawn_aboutwindow()
   local about_window = gui:create_window(100,100,300,300, true, "About")
-  about_window.panel:addwidget(gui:create_labelpanel(20,20,200,200,"sup?"))
+  local memory_panel = gui:create_labelpanel(0,0,100,20,"Memory usage: ??")
+  about_window.panel:addwidget(memory_panel)
+  
+  -- test memory stuff
+  memory_panel.last_update = os.clock()
+  function memory_panel:update()
+    if (os.clock() - self.last_update > 0.1) then
+      memory_panel.last_update = os.clock()
+      memory_panel.label_text = "Memory usage: " .. tostring(app.getmemusage()/1024/1024) .. "MB"
+    end
+  end
   
   gui.windows:add(about_window)
 end
