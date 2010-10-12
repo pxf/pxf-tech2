d1bb9b0a89f69be745d047dc46d1bc097d8c2e46
sweetfish <sweetfish@gmail.com>
2010-10-12 15:54:01 +0200

    Started on DERPComponent stuff

---
+++ Projects/DERPEditor/data/DERPComponents.lua
@@ -0,0 +1,16 @@
+derp_components = {}
+derp_components.aux = {}
+derp_components.render = {}
+derp_components.postprocess = {}
+derp_components.output = {}
+
+-- Aux::Script
+derp_components.aux.script = {}
+function derp_components.aux.script:generate_json(component_data)
+  return "LOL TODO"
+end
+
+function derp_components.aux.script:draw(component_data)
+  -- TODO: draw component in workspace
+end
+
