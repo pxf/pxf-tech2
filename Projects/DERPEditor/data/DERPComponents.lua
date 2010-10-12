4aeca110defdca3691e031349526b7c8d229dd60
sweetfish <sweetfish@gmail.com>
2010-10-12 16:24:36 +0200

    Some progress on component stuff

--- Projects/DERPEditor/data/DERPComponents.lua
+++ Projects/DERPEditor/data/DERPComponents.lua
@@ -1,11 +1,18 @@
 derp_components = {}
-derp_components.aux = {}
-derp_components.render = {}
-derp_components.postprocess = {}
-derp_components.output = {}
+derp_components.aux = {name = "Auxiliary Blocks"}
+--derp_components.render = {}
+--derp_components.postprocess = {}
+--derp_components.output = {}
 
 -- Aux::Script
-derp_components.aux.script = {}
+derp_components.aux.script = { name = "Generic Script Block"
+                             , tooltip = "Create a generic Lua script block."
+                             }
+function derp_components.aux.script:new_block(x,y)
+  local block = { x = x, y = y, w = 100, h = 100, group = "aux", type = "script", inputs = { 0,0,0 }, outputs = { 0,0,0 } }
+  return block
+end
+
 function derp_components.aux.script:generate_json(component_data)
   return "LOL TODO"
 end
@@ -13,4 +20,3 @@ end
 function derp_components.aux.script:draw(component_data)
   -- TODO: draw component in workspace
 end
-
