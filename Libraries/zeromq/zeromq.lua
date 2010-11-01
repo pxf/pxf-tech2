ef72c1307462395998cb64fc95bb2a3b1902d427
jalgelind <jalgelind@gmail.com>
2010-11-01 16:54:32 +0100

    Added zeromq as a library. Build-file needs to be updated to work for MacOSX and other unices.

---
+++ Libraries/zeromq/zeromq.lua
@@ -0,0 +1,25 @@
+Import("../../PxfBuild.lua")
+
+library = NewLibrary("zeromq")
+
+if family == "windows" then
+    library:AddSystemLibrary("ws2_32")
+    library:AddSystemLibrary("winmm")
+    library:AddSystemLibrary("rpcrt4")
+    library:AddDefine("ZMQ_HAVE_WINDOWS")
+    
+    -- Would prefer to not use DLL_EXPORT
+    library:AddDefine("DLL_EXPORT")
+else
+    -- Need any libs for *nix?
+end
+
+library:AddSourceDirectory("sdk/src/*.c")
+library:AddSourceDirectory("sdk/src/*.cpp")
+
+library:AddIncludeDirectory("sdk/src/foreign/xmlParser")
+library:AddSourceDirectory("sdk/src/foreign/xmlParser/*.cpp")
+
+library:AddIncludeDirectory("sdk/include")
+library:AddIncludeDirectory("sdk/src")
+library:AddSourceDirectory("sdk/src/*.c")
