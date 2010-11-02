62103c157e450596307b392dd8aaf4f5778be622
jalgelind <jalgelind@gmail.com>
2010-11-02 18:47:55 +0100

    Updated the 0mqtest-project to use the C++-API instead.

--- Projects/0mqtest/Source/Main.cpp
+++ Projects/0mqtest/Source/Main.cpp
@@ -10,7 +10,7 @@
 #include <cstdio>
 #include <cstdlib>
 
-#include <zmq.h>
+#include <zmq.hpp>
 
 using namespace Pxf;
 
@@ -19,62 +19,44 @@ int main(int argc, char* argv[])
 	Pxf::RandSetSeed(time(NULL));
 	Kernel* kernel = Pxf::Kernel::GetInstance();
 	
-	void* context = zmq_init(1);
+	zmq::context_t context(1);
 	
 	/* Client */
 	if (argc > 1 && StringCompareI(argv[1], "client") == 0)
 	{
-		void *requester = zmq_socket (context, ZMQ_REQ);
-		
-		zmq_connect (requester, "tcp://localhost:50001");
+		zmq::socket_t socket(context, ZMQ_REQ);
+		socket.connect("tcp://localhost:50001");
 		unsigned int request_nbr;
 		for (request_nbr = 0; request_nbr != 10000; request_nbr++)
 		{
-			zmq_msg_t request;
-			zmq_msg_init_data (&request
-							  , "Hello"
-							  , 6
-							  , 0
-							  , 0);
-			zmq_send (requester, &request, 0);
-			zmq_msg_close (&request);
+			zmq::message_t request(6);
+			MemoryCopy((void*) request.data(), "Hello", 6);
+			socket.send(request);
 
-			zmq_msg_t reply;
-			zmq_msg_init (&reply);
-			zmq_recv (requester, &reply, 0);
-			printf ("Received reply %d: [%s]\n", request_nbr, (char *) zmq_msg_data (&reply));
-			zmq_msg_close (&reply);
+			zmq::message_t reply;
+			socket.recv(&reply);
+			printf ("Received reply %d: [%s]\n", request_nbr, (char*)reply.data());
 		}
-	zmq_close (requester);
-	
 	}
 	/* Server */
 	else if (argc > 1 && StringCompareI(argv[1], "server") == 0)
 	{
-		void* responder = zmq_socket(context, ZMQ_REP);
-		zmq_bind(responder, "tcp://*:50001");
+		zmq::socket_t socket(context, ZMQ_REP);
+		socket.bind("tcp://*:50001");
 		
 		while(1)
 		{
 			// Recieve data
-			zmq_msg_t request;
-			zmq_msg_init(&request);
-			zmq_recv(responder, &request, 0);
-			printf("Received request: %s\n", zmq_msg_data(&request));
-			zmq_msg_close(&request);
+			zmq::message_t request;
+			socket.recv(&request);
+			printf("Received request: %s\n", request.data());
 			
 			// Respond
-			zmq_msg_t reply;
-			zmq_msg_init_size (&reply, 6);
-			MemoryCopy((void *) zmq_msg_data (&reply), "World", 6);
-			zmq_send (responder, &reply, 0);
-			zmq_msg_close (&reply);
+			zmq::message_t reply(6);
+			MemoryCopy((void *) reply.data(), "World", 6);
+			socket.send(reply);
 		}
-		
 	}
-	
-	zmq_term(context);
-	
 	return 0;
 }
 
