fe432eba1aa0fb4e118578001dda0746df789608
jalgelind <jalgelind@gmail.com>
2010-11-10 17:56:29 +0000

    Added sonys vectormathlibrary and googles protobuffers. It works for me, your experience may vary.

--- Projects/0mqtest/Build.lua
+++ Projects/0mqtest/Build.lua
@@ -3,6 +3,8 @@ Import("../../PxfBuild.lua")
 project = NewProject("0mqtest")
 
 project:RequireLibrary("zeromq")
+project:RequireLibrary("vectormathlibrary")
+project:RequireLibrary("protobuffers")
 project:AddIncludeDirectory("Include/")
 project:AddSourceDirectory("Source/*.cpp")
 project:Build()
