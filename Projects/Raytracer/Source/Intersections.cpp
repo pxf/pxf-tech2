cd6f1635123483f0b9173d754b80a6ef2bacbb83
sweetfish <sweetfish@gmail.com>
2010-11-11 23:46:23 +0800

    Started work on raytraycer test app

--- Projects/Raytracer/Source/Intersections.cpp
+++ Projects/Raytracer/Source/Intersections.cpp
@@ -1,11 +1,11 @@
 #include "Intersections.h"
 
-bool ray_sphere(Vec3f *c, float r, ray_t *ray, intersection_response_t* resp)
+bool ray_sphere(Pxf::Math::Vec3f *c, float r, ray_t *ray, intersection_response_t* resp)
 {
 	// algorithm from Real-Time Rendering (3rd Edition), p741 (optimized solution)
-	Vec3f l = *c - ray.o;
-	float s = l.dot(ray.d);
-	float l2 = l.dot(l);
+	Pxf::Math::Vec3f l = *c - ray->o;
+	float s = Dot(l,ray->d);
+	float l2 = Dot(l,l);
 	float r2 = r * r;
 	
 	if (s < 0 && l2 > r2)
@@ -25,21 +25,21 @@ bool ray_sphere(Vec3f *c, float r, ray_t *ray, intersection_response_t* resp)
 	else
 		t = s + q;
 	
-	resp.p = ray.o + ray.d * t;
+	resp->p = ray->o + ray->d * t;
 	// TODO: Return normal for intersection also!!
 	return true;
 }
 
-bool ray_plane(Vec3f *c, Vec3f *n, ray_t *ray, intersection_response_t* resp)
+bool ray_plane(Pxf::Math::Vec3f *c, Pxf::Math::Vec3f *n, ray_t *ray, intersection_response_t* resp)
 {
 	// algorithm from http://www.softsurfer.com/Archive/algorithm_0104/algorithm_0104B.htm
-	Vec3f w = ray.o - *c;
-	float t = - (n.dot(w)) / (n.dot(ray.d));
+	Pxf::Math::Vec3f w = ray->o - *c;
+	float t = - Dot(*n,w) / (Dot(*n,ray->d));
 	
 	if (t < 0.0f)
 		return false;
 	
-	resp.p = ray.o + ray.d * t;
+	resp->p = ray->o + ray->d * t;
 	// TODO: Return normal for intersection also!!
 	return true;
 }
--- Projects/Raytracer/Source/Renderer.cpp
+++ Projects/Raytracer/Source/Renderer.cpp
@@ -4,8 +4,51 @@
  * Renders a task, using region etc and storing the data in pic.
  * Returns true if the task was successfully rendered, false otherwise.
  */
-bool render_task(task_detail_t *task, render_result_t *pic)
+bool render_task(task_detail_t *task, batch_blob_t *datablob, render_result_t *pic)
 {
+	// TODO: Check incomming data!
+	// (Don't know what to check atm... :/)
 	
-	return false;
+	if (task->region[0] < 0 || task->region[1] < 0 || task->region[2] < 0 || task->region[3] < 0 ||
+	    task->region[0] >= task->region[2] || task->region[1] >= task->region[3]
+	    // TODO: See if it is outside the "window"
+		 )
+	{
+		Pxf::Message("TaskRenderer", "Task region invalid! [x0: %d, y0: %d, x1: %d, y1: %d]", task->region[0], task->region[1], task->region[2], task->region[3]);
+		return false;
+	}
+	
+	// Init data
+	int region_width = task->region[2] - task->region[0];
+	int region_height = task->region[3] - task->region[1];
+	pic->data = new pixel_data_t[region_width*region_height]; // width * height
+	
+	// Loop through all region pixels
+	for(int y = 0; y < region_height; ++y)
+	{
+		for(int x = 0; x < region_width; ++x)
+		{
+			// Calculate pixel value
+			if (!calculate_pixel(x, y, task, datablob, &pic->data[y*region_width+x]))
+			{
+				Pxf::Message("TaskRenderer", "Error while calculating pixel: [x: %d, y: %d]", x, y);
+				return false;
+			}
+		}
+	}
+	
+	// Return result
+	return true;
+}
+
+bool calculate_pixel(int x, int y, task_detail_t *task, batch_blob_t *datablob, pixel_data_t *pixel)
+{
+	// TODO: Do some real calculations here!
+	int region_width = task->region[2] - task->region[0];
+	int region_height = task->region[3] - task->region[1];
+	
+	pixel->r = (char)(255 * ((float)x / (float)region_width));
+	pixel->g = (char)(255 * ((float)y / (float)region_height));
+	pixel->b = (char)(255 * ((float)task->task_id / (float)task->task_count));
+	return true;
 }
