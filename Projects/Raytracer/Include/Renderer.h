348ddff97328c91a71e3614005b410852118936b
sweetfish <sweetfish@gmail.com>
2010-11-11 16:46:23 +0100

    Started work on raytraycer test app

--- Projects/Raytracer/Include/Renderer.h
+++ Projects/Raytracer/Include/Renderer.h
@@ -1,24 +1,33 @@
 #ifndef _RENDERER_H_
 #define _RENDERER_H_
 
+#include <Pxf/Base/Debug.h>
+
 struct batch_blob_t
 {
-	char lols[256]; // Something stupid.
+	char lols[256]; // Something stupid for now..
 	// TODO: Add textures, kd-tree etc etc.
 };
 
 struct task_detail_t
 {
 	int region[4]; // x0,y0,x1,y1
-	batch_blob_t *datablob;
+	int task_id;
+	int task_count;
 	// TODO: Add camera stuff etc!
+};
+
+struct pixel_data_t
+{
+	char r,g,b;
 };
 
 struct render_result_t
 {
-	char *data;
+	pixel_data_t *data;
 };
 
-bool render_task(task_detail_t *task, render_result_t *pic);
+bool render_task(task_detail_t *task, batch_blob_t *datablob, render_result_t *pic);
+bool calculate_pixel(int x, int y, task_detail_t *task, batch_blob_t *datablob, pixel_data_t *pixel);
 
 #endif /* _RENDERER_H_ */
--- Projects/Raytracer/Source/Main.cpp
+++ Projects/Raytracer/Source/Main.cpp
@@ -9,12 +9,16 @@
 #include <Pxf/Graphics/GraphicsDevice.h>
 #include <Pxf/Graphics/Window.h>
 #include <Pxf/Graphics/WindowSpecifications.h>
+#include <Pxf/Graphics/Texture.h>
+#include <Pxf/Graphics/PrimitiveBatch.h>
 #include <Pxf/Input/InputDevice.h>
 #include <ctime>
 #include <cstdio>
 #include <cstdlib>
+#include "Renderer.h"
 
 using namespace Pxf;
+using namespace Graphics;
 
 int main(int argc, char* argv[])
 {
@@ -38,15 +42,78 @@ int main(int argc, char* argv[])
 	spec.VerticalSync = true;
 	Graphics::Window* win = gfx->OpenWindow(&spec);
 	
+	// Generate awesome red output buffer
+	int w = 512;
+	int h = 512;
+	int channels = 3;
+	const int task_count = 8;
+	int task_size_w = w / task_count;
+	int task_size_h = h / task_count;
+	char pixels[w*h*channels];
+	
+	// job specifics
+	batch_blob_t blob;
+	task_detail_t task;
+	task.task_count = task_count*task_count;
+	
+	// create textures and primitive batches
+	Texture *region_textures[task_count*task_count] = {0};
+	
+	for(int ty = 0; ty < task_count; ++ty)
+	{
+		for(int tx = 0; tx < task_count; ++tx)
+		{
+			task.region[0] = tx*task_size_w;
+			task.region[1] = ty*task_size_h;
+			task.region[2] = tx*task_size_w+task_size_w;
+			task.region[3] = ty*task_size_h+task_size_h;
+			task.task_id = ty*task_count+tx;
+			
+			render_result_t pixel_region;
+			if (!render_task(&task, &blob, &pixel_region))
+			{
+				Pxf::Message("Main", "Error while trying to render task: [tx: %d, ty: %d]", tx, ty);
+			} else {
+				// Success
+				region_textures[ty*task_count+tx] = Pxf::Kernel::GetInstance()->GetGraphicsDevice()->CreateTextureFromData((const unsigned char*)pixel_region.data, task_size_w, task_size_w, channels);
+			}
+		}
+	}
+	
+	PrimitiveBatch *pbatch = new PrimitiveBatch(Pxf::Kernel::GetInstance()->GetGraphicsDevice());
+	
 	while(win->IsOpen())
 	{
 		inp->Update();
 		if (inp->GetLastKey() == Input::ESC)
 			break;
 			
+		// Setup view!!!!!!!!
+		Math::Mat4 prjmat = Math::Mat4::Ortho(0, win->GetWidth(), 0, win->GetHeight(), -0.1f, 100.0f);
+	  gfx->SetProjection(&prjmat);
+		
+		for(int ty = 0; ty < task_count; ++ty)
+		{
+			for(int tx = 0; tx < task_count; ++tx)
+			{
+				if (region_textures[ty*task_count+tx])
+				{
+					// Bind texture
+					Pxf::Kernel::GetInstance()->GetGraphicsDevice()->BindTexture(region_textures[ty*task_count+tx]);
+					
+					// Setup quad
+					pbatch->QuadsBegin();
+					pbatch->QuadsDrawTopLeft(tx*task_size_w, ty*task_size_h, task_size_w, task_size_w);
+					pbatch->QuadsEnd();
+				}
+			}
+		}
+		
 		inp->ClearLastKey();
 		win->Swap();
 	}
+	
+	delete pbatch;
 	
 	return 0;
 }
