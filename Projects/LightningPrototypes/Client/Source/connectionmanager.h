a795b797693d94c2284f957d7cbdb323560a5406
Filip Lundborg <filip@filiplundborg.se>
2010-11-24 15:08:12 +0100

    Changed the returntype on select to Packet. Changed from CamelCase to.... small letters?

--- Projects/LightningPrototypes/Client/Source/connectionmanager.h
+++ Projects/LightningPrototypes/Client/Source/connectionmanager.h
@@ -4,7 +4,7 @@
 #include <Pxf/Util/Array.h>
 
 enum ConnectionType {
-	CLIENT, TRACKER, INERNAL
+	CLIENT, TRACKER, INTERNAL
 };
 
 struct Connection {
@@ -15,25 +15,32 @@ struct Connection {
 	ConnectionType type;
 };
 
+struct Packet {
+	Connection *connection;
+	int length;
+	char *data;
+};
+
 class ConnectionManager
 {
 	private:
 		Pxf::Util::Array<struct Connection *> m_Connections;
 
 	public:
-		int NewConnection();
-		bool BindConnection(int _id, char *_address, int _port);
-		bool ConnectConnection(int _id, char *_address, int _port);
+		int new_connection();
+		bool bind_connection(int _id, char *_address, int _port);
+		bool connect_connection(int _id, char *_address, int _port);
 		
-		int session_id2id(int _session_id);
-		int id2session_id(int _id);
-		int id2socket(int _id);
+		//int session_id2id(int _session_id);
+		//int id2session_id(int _id);
+		//int id2socket(int _id);
+		//int session_id2socket(int _session_id);
 		
-		struct Connection *Select();
+		Packet *select();
 
-		bool Send(struct Connection *_connection, char *_msg, int _length);
+		bool send(Connection *_connection, char *_msg, int _length);
 		//bool Send(int _session_id, _msg, _length);
-		bool Send(int _id, char *_msg, int _length);
+		bool send(int _id, char *_msg, int _length);
 };
 
 #endif  /* _CONNECTIONMANAGER__H_ */
