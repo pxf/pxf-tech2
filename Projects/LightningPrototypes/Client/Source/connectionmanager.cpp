bf5d027f523d518a01a21b92232fef4d2d0a15ae
ingemaradahl <ingemar.adahl@gmail.com>
2010-11-24 15:37:06 +0100

    Send wrapper.

--- Projects/LightningPrototypes/Client/Source/connectionmanager.cpp
+++ Projects/LightningPrototypes/Client/Source/connectionmanager.cpp
@@ -1,3 +1,13 @@
+#if defined(CONF_FAMILY_UNIX)
+#include <sys/socket.h>
+#endif
+
 #include "connectionmanager.h"
+
+bool ConnectionManager::Send(Connection *_connection, char *_msg, int _length)
+{
+	// Blocking as hell
+	if (send(_connection->socket, _msg, length, 0) == -1) return false;
+}
 
 
