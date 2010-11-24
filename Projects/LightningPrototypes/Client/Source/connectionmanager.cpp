85ac96fb6596c6d62d4ae59465df9fd6f97154f9
Filip Lundborg <filip@filiplundborg.se>
2010-11-24 15:28:53 +0100

    Added empty functions for ConnectionManager.

--- Projects/LightningPrototypes/Client/Source/connectionmanager.cpp
+++ Projects/LightningPrototypes/Client/Source/connectionmanager.cpp
@@ -1,3 +1,31 @@
 #include "connectionmanager.h"
 
+Connection *ConnectionManager::new_connection(ConnectionType _type)
+{
+	return NULL;
+}
 
+bool ConnectionManager::bind_connection(Connection *_connection, char *_address, int _port)
+{
+	return false;
+}
+
+bool ConnectionManager::connect_connection(Connection *_connection, char *_address, int _port)
+{
+	return false;
+}
+
+Packet *ConnectionManager::recv()
+{
+	return NULL;
+}
+
+bool ConnectionManager::send(Connection *_connection, char *_msg, int _length)
+{
+	return false;
+}
+
+bool ConnectionManager::send(int _id, char *_msg, int _length, bool _is_session_id)
+{
+	return false;
+}
