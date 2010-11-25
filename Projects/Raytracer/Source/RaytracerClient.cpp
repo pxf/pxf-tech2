e39881295bc4e1a03939df1ff3e980cb7c58cca9
jalgelind <jalgelind@gmail.com>
2010-11-25 14:32:28 +0100

    Added skeleton for the raytracer client.

---
+++ Projects/Raytracer/Source/RaytracerClient.cpp
@@ -0,0 +1,25 @@
+#include <RaytracerClient.h>
+
+RaytracerClient::RaytracerClient()
+{
+
+}
+
+RaytracerClient::~RaytracerClient()
+{
+
+}
+
+bool RaytracerClient::Run()
+{
+
+	// wait for jobs
+
+	// add jobs to queue
+
+	// do <numcpu> jobs taken from queue
+
+	// send back results (combine with above step if possible)
+
+	return false;
+}
