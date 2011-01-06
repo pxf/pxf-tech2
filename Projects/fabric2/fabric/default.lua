fefdfa4c118d91d6c914a9730a6dc06cd2a96713
sweetfish <sweetfish@gmail.com>
2011-01-06 03:48:05 +0100

    added a sinwaveluls

--- Projects/fabric2/fabric/default.lua
+++ Projects/fabric2/fabric/default.lua
@@ -10,31 +10,12 @@ settings:save()
 -- initial draw
 gfx.redrawneeded()
 
--- shadertest
-local testshader = gfx.createshader("testshader", [[
-
-void main() {
-  gl_TexCoord[0] = gl_MultiTexCoord0;
-	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-}
-
-
-]], [[
-uniform sampler2D tex;
-void main()
-{
-	// Setting Each Pixel To Red
-	vec4 color = texture2D(tex, gl_TexCoord[0].st);
-	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0) * color;
-}
-
-
-]])
-
-local testtex = gfx.newtexture(12, 128, 128)
-local testfbo = gfx.newframebuffer()
-testfbo:attach(testtex, 1)
+----------------------------------------------
+-- logo animation
 local a = 0
+local logo = gfx.loadtexture(512, "fabric/logo.png")
+
+gfx.setclearcolor(30/256, 0, 0)
 
 function update()
   if inp.iskeydown(inp.ESC) then
@@ -47,26 +28,9 @@ function update()
 end
 
 function draw(force)
-  
-  gfx.bindframebuffer(testfbo)
+  gfx.bindtexture(logo)
   gfx.clear()
-  gfx.setview(0,0,128,128)
-  gfx.setortho(0,128,0,128)
-  gfx.bindshader(testshader)
-  
-  gfx.bindtexture(runtimeerror_tex)
-  gfx.translate(64, 64)
-  gfx.rotate(a)
-  gfx.drawcentered(0, 0, 128, 128)
-  
-  gfx.bindshader()
-  gfx.bindframebuffer()
-  
-  gfx.loadidentity()
-  gfx.clear()
-  gfx.bindtexture(testtex)
-  gfx.drawcentered(app.width / 2, app.height / 2 - 128,256,256)
-  gfx.drawcentered(app.width / 2, app.height / 2,256,256)
-  gfx.drawcentered(128 + math.cos(a)*32, 128 + math.sin(a)*32,256,256)
-
+  for i=1,512 do
+    gfx.drawcentered(i-1, app.height / 2 + math.sin(a-i*0.01) * 64 + math.cos((a-i*0.01)*0.6)*128, 1, 64, i-1+a*100, 0, 1, 64)
+  end
 end
